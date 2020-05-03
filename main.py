7bb72620ed176bc840ef178d8016bb0cf2e3196b
gzimin <glebzimin@yahoo.com>
2020-05-03 14:24:31 +0400

    Total rework getting recent items
    * Move captcha check to one function, now it will open browser
    * Small fixes in filtering data function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 
+
 from bs4 import BeautifulSoup
+
+import webbrowser
 
 # Constant name of items quality
 QUALITIES = {
@@ -20,6 +23,16 @@ DIV_QUALITIES = {
     }
 
 
+def check_for_captcha(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    captcha = soup.find("div", {"class": "g-recaptcha"})
+    if captcha:
+        print("We catch captcha, go here if browser didn't open: {}".format(url))
+        webbrowser.open(url, new=2)
+        input("Press ENTER:")
+
+
 def calc_maximum_pages(items_url):
     # Set very big num to page to get real last page
     if "&page=" in items_url:
@@ -29,12 +42,7 @@ def calc_maximum_pages(items_url):
     else:
         items_url = items_url + "&page=100000000"
 
-    page = requests.get(items_url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    captcha = soup.find("div", {"class": "g-recaptcha"})
-    if captcha:
-        print("We catch captcha, go here: {}".format(items_url))
-        input("Press ENTER:")
+    check_for_captcha(items_url)
     page = requests.get(items_url)
     soup = BeautifulSoup(page.text, 'html.parser')
     num_page = soup.find("li", {"class": "active"})
@@ -43,67 +51,17 @@ def calc_maximum_pages(items_url):
 
 
 def filtering_data(value, num_type=None, digit=True):
-    if num_type is None or num_type in value.text:
+    if not isinstance(value, str):
         value = value.text
-        if num_type:
-            value = value.replace(num_type, "")
-        value = value.replace("\n", "")
-        value = value.replace("\r", "")
-        value = value.replace(" ", "")
-        value = value.replace(",", "")
-        if digit:
-            if "." in value:
-                value = value.split(".")[0]
-            value = int(value)
-        return value
-    return False
-
-
-def alternate_get_item_names(soup, url):
-    item_quality = ''
-    for key, value in QUALITIES.items():
-        if value in url:
-            db_filename = url + "_" + key
-            item_quality = DIV_QUALITIES[key]
-            break
-    captcha = soup.find("div", {"class": "g-recaptcha"})
-    if captcha:
-        print("We catch captcha, go here: {}".format(url))
-        print("Then press ENTER")
-        input()
-    all_names_list = []
-    all_names = soup.findAll("div", {"class": item_quality})
-    for name in all_names:
-        name = name.text.strip()
-        all_names_list.append(name)
-    return all_names_list
-
-
-def get_item_names(soup, items_url):
-    # Here we takes names of the products, return list of names
-    trade_table = soup.find(class_="trade-list-table")
-    item_names = []
-    try:
-        trade_table.tbody
-    except AttributeError:
-        print("Seems like we catch captcha, check {}".format(items_url))
-        print("If there is a captcha, pass it, than type OK")
-        ok = input()
-        if ok == "OK":
-            page = requests.get(items_url)
-            soup = BeautifulSoup(page.text, 'html.parser')
-            trade_table = soup.find(class_="trade-list-table")
-            pass
-        else:
-            print("Trying to get trade table with another class\n")
-            trade_table = soup.find(class_="trade-list-table max-width")
-    for column in trade_table.tbody:
-        try:
-            name = column.div.text.strip()
-            item_names.append(name)
-        except AttributeError:
-            pass
-    return item_names
+    value = value.replace("\n", "")
+    value = value.replace("\r", "")
+    value = value.replace(" ", "")
+    value = value.replace(",", "")
+    if digit:
+        if "." in value:
+            value = value.split(".")[0]
+        value = int(value)
+    return value
 
 
 def get_items_with_suggested_price(base_items_url, page_count=None):
@@ -140,11 +98,7 @@ def get_items_with_suggested_price(base_items_url, page_count=None):
         soup = BeautifulSoup(page.text, 'html.parser')
 
         # Check for captcha
-        if not soup.find("table", {"class": "trade-list-table"}):
-            print("We catch captcha, go to:\n {}".format(base_items_url))
-            input("Then press Enter here:")
-            page = requests.get(base_items_url)
-            soup = BeautifulSoup(page.text, 'html.parser')
+        check_for_captcha(base_items_url)
 
         # Get all body elements
         tbody_elem = soup.find("tbody")
@@ -175,11 +129,8 @@ def get_suggested_items_from_db(filename):
     return suggested_items_dict
 
 
-def recent_items_calc(recent_items_url, suggested_items_dict, matches_db_filename, page_count, coef):
+def gather_recent_items(recent_items_url, page_count, coef, suggested_items_db):
 
-    with open(matches_db_filename, "w") as matches_db:
-        matches_db.write("{:>50} {:>25} {:>30} {:>30} {:>33}\n".format(
-            "Item name", "Current Price", "Suggested Price", "Location", "User"))
     # Check page url format
     if "&page=" in recent_items_url:
         page_num_length = len(recent_items_url.split("&page=")[1])
@@ -187,60 +138,53 @@ def recent_items_calc(recent_items_url, suggested_items_dict, matches_db_filenam
     else:
         recent_items_url = recent_items_url + "&page=1"
 
+    # Set items quality and matches filename
+    item_quality = ''
+    matches_db_filename = "matches.txt"
+    for key, value in QUALITIES.items():
+        if value in recent_items_url:
+            matches_db_filename = "matches_" + key + '.txt'
+            item_quality = DIV_QUALITIES[key]
+            break
+
+    # Write titles to the file
+    with open(matches_db_filename, "w") as matches_db:
+        matches_db.write("{:>50} {:>25} {:>30} {:>30} {:>33}\n".format(
+            "Item name", "Current Price", "Suggested Price", "Location", "User"))
+
+    # Gather all information
     for i in range(1, page_count + 1):
         recent_items_url = recent_items_url.replace("&page=" + str(i), "&page=" + str(i + 1))
+        check_for_captcha(recent_items_url)
         page = requests.get(recent_items_url)
         soup = BeautifulSoup(page.text, 'html.parser')
+        all_items = soup.findAll("tr", {"class": "cursor-pointer"})
 
-        # Get names of items
-        item_names = alternate_get_item_names(soup, recent_items_url)
+        for item in all_items:
+            name = item.find("div", {"class": item_quality}).text.strip()
+            if name in suggested_items_db.keys():
+                price = filtering_data(item.find("td", {"class": "gold-amount bold"}).text.split("X")[0], digit=True)
+                if suggested_items_db.get(name) > float(price) * coef:
 
-        # Get trader and location info
-        # This fields have the same class of element, we need to get them all and then sort it like list of tuples
-        trader_or_location_list_unsorted = []
-        trader_location_list_sorted = []
-        traders_and_locations = soup.findAll("td", {"class": "hidden-xs"})
-        for trader_or_location in traders_and_locations:
-            trader_or_location = filtering_data(trader_or_location, num_type=None, digit=False)
-            if trader_or_location != '':
-                trader_or_location_list_unsorted.append(trader_or_location)
+                    # If we found something, then we can take location and trader info
+                    trader_and_locations = item.findAll("td", {"class": "hidden-xs"})
+                    location = ""
+                    trader_name = ""
 
-        for ind in range(0, len(trader_or_location_list_unsorted) - 1, 2):
-            trader_location_list_sorted.append((trader_or_location_list_unsorted[ind + 1],
-                                                trader_or_location_list_unsorted[ind]))
+                    for index in range(1, len(trader_and_locations) - 1):
+                        location += filtering_data(trader_and_locations[index].text.strip(), digit=False)
+                    trader_name = item.find("div", {"class": "text-small-width"}).text.strip()
 
-        flag = False
-        # Get prices and create new db file with matches
-        all_prices = soup.findAll("td", {"class": "gold-amount bold"})
-        for price, item_name in zip(all_prices, item_names):
-            if suggested_items_dict.get(item_name):
-                price = filtering_data(price, num_type=None, digit=False)
-                price = price.split('X')[0]
-                if suggested_items_dict.get(item_name) > float(price) * coef:
-                    index = item_names.index(item_name)
-                    trader_name = trader_location_list_sorted[index][0]
-                    trader_location = trader_location_list_sorted[index][1]
                     with open(matches_db_filename, "a") as matches_db:
                         matches_db.write(
                             "{:>50} {:>20} {:>30} {:>50} {:>20}\n".format(
-                                item_name, price, suggested_items_dict.get(item_name), trader_name, trader_location))
-                    flag = True
-
-        print("Page {}/{} is done!".format(str(i), str(page_count)))
-        if flag:
-            print("We found something! Check db")
-        else:
-            print("Empty...")
-        # time.sleep(5)
-    print("Search done, check {} file".format(matches_db_filename))
+                                name, price, suggested_items_db.get(name), location, trader_name))
+                    print("Found something! Check {} file".format(matches_db_filename))
+        print("Page {}/{} done!".format(i, page_count))
 
 
 def main():
-    coefficient = 1.5
-    page_count = 50
-
-    print("Collect suggested items or recent? (1/2)")
-    answer = input()
+    answer = input("Collect suggested items or recent? (1/2): ")
     if answer == "1":
         items_url = input("Paste url with suggested items: ")
         page_ans = input("Calc all pages or insert num of pages(Y/Number): ")
@@ -250,12 +194,15 @@ def main():
         else:
             get_items_with_suggested_price(items_url, page_count=int(page_ans))
     elif answer == "2":
-        filename = input("Insert filename of db with suggested items:")
-        suggested_items_dict = get_suggested_items_from_db(filename)
-        recent_items_url = input("Paste link with items:")
-        print("Searching with coefficient: {}, total pages: {}\n".format(coefficient, page_count))
-        recent_items_calc(recent_items_url=recent_items_url, suggested_items_dict=suggested_items_dict,
-                          matches_db_filename="matches_db.txt", coef=coefficient, page_count=page_count)
+        # TODO: Make check of this file
+        db_filename = input("Insert filename of db with suggested items: ")
+        # Get suggested items from db
+        suggested_items_db = get_suggested_items_from_db(db_filename)
+        recent_items_url = input("Paste link with items (Make sure you choose Item Quality): ")
+        coef = float(input("Write coef of benefit (example 1.5): "))
+        max_pages = calc_maximum_pages(recent_items_url)
+        page_count = int(input("Write count of pages for scan (for this link max is {}): ".format(max_pages)))
+        gather_recent_items(recent_items_url, page_count, coef, suggested_items_db)
     else:
         print("Wrong answer, try again...")
         return 0
